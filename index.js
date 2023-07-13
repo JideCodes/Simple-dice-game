0263b851e8e90de56bc8539d1524b7fa7a8e925a
JideCodes <99658115+JideCodes@users.noreply.github.com>
2023-07-13 05:34:48 +0100

    Add files via upload
    This update allows users use a button to roll the dice instead of having to refresh the page every time for a new roll.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-let randomNumber1 = Math.floor(Math.random() * 6 + 1);
-let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+document.querySelector("button").addEventListener("click", function diceRoll(){
 
-document.querySelector(".img1").setAttribute("src", "images/" + "dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/" +  "dice" + randomNumber2 + ".png");
+    let randomNumber1 = Math.floor(Math.random() * 6 + 1); //generate a random number for first dice
+    let randomNumber2 = Math.floor(Math.random() * 6 + 1); //do same for second dice
 
-if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").textContent = "Player 1 wins! 🧓🏿";
-} else if (randomNumber1 === randomNumber2){
-    document.querySelector("h1").textContent = "Draw!\n👵🏽🤝🏾🧓🏿";
-} else{
-    document.querySelector("h1").textContent = "Player 2 wins! 👵🏽";
-}
+    document.querySelector(".img1").setAttribute("src", "images/" + "dice" + randomNumber1 + ".png"); //this sets changes the src attribute on the img1 class to the concatenated strings plus the random number generated
+    document.querySelector(".img2").setAttribute("src", "images/" +  "dice" + randomNumber2 + ".png"); //this does the above for the img2 class too
+
+    //this block then checks for the winner between the randomized numbers above and dynamically changes the result on the H1 text on the page
+    if (randomNumber1 > randomNumber2){
+        document.querySelector("h1").textContent = "Player 1 wins! 🧓🏿";
+    } else if (randomNumber1 === randomNumber2){
+        document.querySelector("h1").textContent = "Draw!\n👵🏽🤝🏾🧓🏿";
+    } else{
+        document.querySelector("h1").textContent = "Player 2 wins! 👵🏽";
+    }
+})
